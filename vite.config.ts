242302a7c0722aab8469085cf58c3f2648e0ebe0
DrgDodge <dragonul530@gmail.com>
2024-10-18 20:58:14 +0300

    Port 4000

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		host: 'localhost',
-		port: '4000',
-		strictPort: true,
-	}
+	plugins: [sveltekit()]
 });
